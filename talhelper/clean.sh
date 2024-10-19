4bbf2abf9f7bd25daff34a16ec21692f07a38abe
QJoly <quentinj@une-pause-cafe.fr>
2024-10-20 00:49:15 +0200

    Refactor clean.sh and run.sh scripts for Talos cluster reset and apply-config

--- talhelper/clean.sh
+++ talhelper/clean.sh
@@ -8,8 +8,11 @@
 # hide the evidence
 clear
 
+pe "# Reset the cluster \n"
+pe "talhelper gencommand reset"
 
-pei "talosctl reset --talosconfig=./clusterconfig/talosconfig --nodes=192.168.1.101 --graceful=false --reboot;
-talosctl reset --talosconfig=./clusterconfig/talosconfig --nodes=192.168.1.102 --graceful=false --reboot;
-talosctl reset --talosconfig=./clusterconfig/talosconfig --nodes=192.168.1.103 --graceful=false --reboot;
+fpe "talosctl reset --talosconfig=./clusterconfig/talosconfig --nodes=192.168.1.101 --graceful=false --reboot &
+talosctl reset --talosconfig=./clusterconfig/talosconfig --nodes=192.168.1.102 --graceful=false --reboot &
+talosctl reset --talosconfig=./clusterconfig/talosconfig --nodes=192.168.1.103 --graceful=false --reboot &
 talosctl reset --talosconfig=./clusterconfig/talosconfig --nodes=192.168.1.114 --graceful=false --reboot;"
+
--- talhelper/run.sh
+++ talhelper/run.sh
@@ -4,25 +4,26 @@
 # include the magic    #
 ########################
 . ../common/demo-magic.sh
-
 # hide the evidence
 clear
 
 pei "talhelper gensecret > talsecret.yaml"
 pei "ls -l talsecret.yaml"
 pe "talhelper genconfig"
-pe "talhelper gencommand apply"
+p "talhelper gencommand"
+talhelper gencommand | grep "Generate talosctl"
 
-pei "talhelper gencommand apply --extra-flags --insecure"
-pe "talosctl apply-config --talosconfig=./clusterconfig/talosconfig --nodes=192.168.1.101 --file=./clusterconfig/talos-cluster-cp-1.yaml --insecure;
-talosctl apply-config --talosconfig=./clusterconfig/talosconfig --nodes=192.168.1.102 --file=./clusterconfig/talos-cluster-cp-2.yaml --insecure;
-talosctl apply-config --talosconfig=./clusterconfig/talosconfig --nodes=192.168.1.103 --file=./clusterconfig/talos-cluster-cp-3.yaml --insecure;
-talosctl apply-config --talosconfig=./clusterconfig/talosconfig --nodes=192.168.1.114 --file=./clusterconfig/talos-cluster-worker-1.yaml --insecure;"
+pe "talhelper gencommand apply --extra-flags --insecure"
+fpe 'talosctl apply-config --talosconfig=./clusterconfig/talosconfig --nodes=192.168.1.101 --file=./clusterconfig/talos-cluster-cp-1.yaml --insecure &
+ talosctl apply-config --talosconfig=./clusterconfig/talosconfig --nodes=192.168.1.102 --file=./clusterconfig/talos-cluster-cp-2.yaml --insecure &
+ talosctl apply-config --talosconfig=./clusterconfig/talosconfig --nodes=192.168.1.103 --file=./clusterconfig/talos-cluster-cp-3.yaml --insecure &
+ talosctl apply-config --talosconfig=./clusterconfig/talosconfig --nodes=192.168.1.114 --file=./clusterconfig/talos-cluster-worker-1.yaml --insecure;'
+
 pe "talhelper gencommand bootstrap"
-pe "talosctl bootstrap --talosconfig=./clusterconfig/talosconfig --nodes=192.168.1.101"
+fpe "talosctl bootstrap --talosconfig=./clusterconfig/talosconfig --nodes=192.168.1.101"
 
 pei "talhelper gencommand kubeconfig --extra-flags --merge=false"
-pe "talosctl kubeconfig --talosconfig=./clusterconfig/talosconfig --merge=false --nodes=192.168.1.101;"
+fpe "talosctl kubeconfig --talosconfig=./clusterconfig/talosconfig --merge=false --nodes=192.168.1.101 --force;"
 
 pe "kubectl --kubeconfig ./kubeconfig get pods -A"
 
